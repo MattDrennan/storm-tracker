bb72f9af3c7957bb4e7e91a982f1c1109d447ab5
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2022-10-05 12:29:24 -0400

    Server Clean Up

--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ const app = express();
 var mysql = require('mysql');
 require('dotenv').config();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 var nodemailer = require('nodemailer');
 
 /**
@@ -70,31 +68,6 @@ conn.connect((err) => {
     if (err) throw err;
     console.log('Mysql Connected!');
 });
-
-/**
- * Bcrypt Password Variables
-*/
-const saltRounds = 10;
-
-/**
- * Verifys the user is valid using JWT
- */
-const verifyJWT = (req, res, next) => {
-    const token = req.cookies["access-token"];
-
-    if (!token) {
-        //return res.status(400).json({ error: "User Not Authenticated!" });
-    } else {
-        jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
-            if (err) {
-                //return res.status(400).json({ error: err });
-            } else {
-                req.authenticated = true;
-                next();
-            }
-        });
-    }
-}
 
 /**
  * Get icons that can be used
@@ -167,160 +140,6 @@ app.get("/search", (req, res) => {
             res.json({ 'success': true, 'result': false });
         }
     });
-});
-
-/**
-* Logs in the user
-*/
-app.post("/login", (req, res) => {
-    // Query
-    let sql = 'SELECT * FROM users WHERE email = "' + req.body.info.email + '"';
-    let query = conn.query(sql, (err, results, fields) => {
-        if (results.length > 0) {
-            // Check password
-            bcrypt.compare(req.body.info.password, results[0].password, function (err, result) {
-                if (result) {
-                    const id = results[0].id;
-                    const token = jwt.sign({ id }, process.env.SESSION_SECRET, {
-                        expiresIn: 300,
-                    });
-                    const accountType = results[0].type;
-
-                    // Token
-                    res.cookie("access-token", token, {
-                        maxAge: 60 * 60 * 24 * 30 * 1000,
-                        httpOnly: true,
-                    });
-
-                    // User ID
-                    res.cookie("id", id, {
-                        maxAge: 60 * 60 * 24 * 30 * 1000,
-                        httpOnly: true,
-                    });
-
-                    // Admin
-                    res.cookie("accountType", accountType, {
-                        maxAge: 60 * 60 * 24 * 30 * 1000,
-                        httpOnly: true,
-                    });
-
-                    res.json({ auth: true, token: token, result: results });
-                } else {
-                    res.json({ auth: false, message: "Incorrect e-mail or password." });
-                }
-            });
-        } else {
-            res.json({ auth: false, message: "Incorrect e-mail or password." });
-        }
-    });
-});
-
-/**
- * Registers the user into the database
-*/
-app.post("/register", (req, res) => {
-    // Hash password
-    bcrypt.hash("" + req.body.password, saltRounds, function (err, hash) {
-        let data = { email: req.body.email, password: hash };
-
-        let sql = 'INSERT INTO users SET ?';
-        let query = conn.query(sql, data, (err, results) => {
-            if (err) throw err;
-            res.json({ 'success': true, 'response': results });
-        });
-    });
-});
-
-/**
- * Check if e-mail exists
-*/
-app.get("/checkuser", (req, res) => {
-    let sql = 'SELECT * FROM users WHERE email = "' + req.query.email + '"';
-    let query = conn.query(sql, (err, results) => {
-        if (err) throw err;
-        if (results.length > 0) {
-            res.json({ 'success': true, 'result': true });
-        } else {
-            res.json({ 'success': true, 'result': false });
-        }
-    });
-});
-
-/**
- * Send recovery e-mail to user
-*/
-app.post("/forgot", (req, res) => {
-    // Query - Get user
-    let sql = 'SELECT * FROM users WHERE email = "' + req.body.email + '"';
-    let query = conn.query(sql, (err, results) => {
-        if (err) throw err;
-        if (results.length > 0) {
-            let random = Math.floor(Math.random() * 9999) + 1000;
-
-            // Hash password
-            bcrypt.hash("" + random, saltRounds, function (err, hash) {
-                // Query - Update user password
-                let sql2 = 'UPDATE users SET password = "' + hash + '" WHERE email = "' + req.body.email + '"';
-                let query2 = conn.query(sql2);
-
-                var mailOptions = {
-                    from: process.env.EMAIL_FROM,
-                    to: req.body.email,
-                    subject: 'Account Recovery',
-                    text: 'E-mail: ' + req.body.email + '\n\nPassword: ' + random + ''
-                };
-
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        res.json({ 'success': true, 'response': error });
-                    } else {
-                        res.json({ 'success': true, 'response': info.response });
-                    }
-                });
-            });
-        } else {
-            res.json({ 'success': true, 'message': "Unable to locate an account with this e-mail address." });
-        }
-    });
-});
-
-/**
- * Send recovery e-mail to user
-*/
-app.post("/changepass", verifyJWT, (req, res) => {
-    // Hash password
-    bcrypt.hash("" + req.body.newpassword, saltRounds, function (err, hash) {
-        // Query
-        let sql = 'UPDATE users SET password = "' + hash + '" WHERE id = "' + req.cookies["id"] + '"';
-        let query = conn.query(sql, (err, results, fields) => {
-            res.json({ 'success': true, result: results });
-        });
-    });
-});
-
-/**
- * Logs the user out
-*/
-app.post("/logout", verifyJWT, (req, res) => {
-    if (req.cookies["access-token"] && req.cookies["id"]) {
-        // Destroy cookies
-        res.clearCookie("access-token");
-        res.clearCookie("id");
-        res.clearCookie("accountType");
-        res.end();
-    }
-});
-
-/**
- * Get's if user is authenticated
-*/
-app.get("/isUserAuth", verifyJWT, (req, res) => {
-    if (req.cookies["access-token"] && req.cookies["id"]) {
-        res.json({ 'loggedIn': true, 'accountType': req.cookies["accountType"] });
-    }
-    else {
-        res.json({ 'loggedIn': false });
-    }
 });
 
 /**
